7bc160ed90e22fbc07d74c5bbc3c2f6726936671
Zainch032 <zc19398@gmail.com>
2024-09-27 11:14:38 -0700

    make_payment.py

--- make_payment.py
+++ make_payment.py
@@ -1,5 +1,5 @@
 class Product:
-  def __init__(self, name, amount, price):
+  def _init_(self, name, amount, price):
       self.name = name
       self.amount = amount
       self.price = price
@@ -18,6 +18,23 @@ class Product:
         return total_price
 
 
-  def make_payment(self):
-         pass
+  def make_purchase(self, quantity):
+      try:
+        if quantity > self.amount:
+            raise ValueError(f'Not enough product available. Quantity must be less than {self.amount}')
+        elif quantity < 0:
+            raise ValueError('Quantity must be greater than 0.')
+        self.amount = self.amount - quantity
+        price = self.get_price(quantity)
+        print(f'Here\'s the total price charged: {price}')
+      except ValueError as e:
+         print(f'Error:{e}')
+        
+# create product object
+nose_pin = Product('Dimond Nose Pin', 50, 31000)
+# make purchases against different product quantities (make sure to run each test case)
+nose_pin.make_purchase(2)
+# do not forget to handle exceptions
+# print the remaining stock after each purchase
+print(f'Remaining Stock: {nose_pin.amount}')
 
