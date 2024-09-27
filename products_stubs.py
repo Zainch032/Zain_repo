08044879b04f4426f0a55bf2cc79f7d7005f76cc
Zainch032 <zc19398@gmail.com>
2024-09-27 09:12:56 -0700

    File added

---
+++ products_stubs.py
@@ -0,0 +1,14 @@
+# Muhammad zain (Sp23-bai-032) Maintainer
+# Zaimal Zia (Sp23-bai-040)    Collaborator
+
+class Product:
+  def __init__(self, name, amount, price):
+      self.name = name
+      self.amount = amount
+      self.price = price
+
+  def get_price(self, quantity):
+      pass
+
+  def make_purchase(self, quantity):
+      pass
